24f66f400c539f8b39f92e8d4221b61c84c31745
aurel fontaine <ajfame11@gmail.com>
2020-06-08 21:40:31 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,6 @@
 class AddGradeAndBirthdateToStudents
   def change
-    Add a new INTEGER column named :grade to :students
-    Add a new STRING column named :birthdate to :students
+    Add a new integer column named :grade to :students
+    Add a new string column named :birthdate to :students
   end
 end
